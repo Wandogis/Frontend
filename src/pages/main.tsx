a7c10ea66315f8392fb7cd2b3c601b531f34d789
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-05-11 23:36:56 +0900

    [Fix] main페이지 코드 수정

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,13 +2,13 @@ import MainHeader from "../components/main-header";
 import theme from "../assets/theme";
 // import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
+import MainMenuBar from "../components/main-menu-bar";
 
 const Main: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <MainHeader />
-      </div>
+      <MainHeader />
+      <MainMenuBar />
     </ThemeProvider>
   );
 };
