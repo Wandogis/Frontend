1ea53423a2a6ac66d0966259b1c8d8febfd1328a
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-05-19 17:18:47 +0900

    [Feat] 데이터 캘린더에 불러오기

--- src/components/Calendar/calendar-profile.tsx
+++ src/components/Calendar/calendar-profile.tsx
@@ -4,6 +4,7 @@ import RenderHeader from "./header";
 import { styled } from "styled-components";
 import RenderDays from "./days";
 import RenderCells from "./cells";
+
 const ProfilCalWrapper = styled.div`
   width: 30%;
   margin: 1.5rem 3rem 0 1rem;
@@ -19,18 +20,25 @@ const CalendarWrapper = styled.div``;
 
 export const Calender: React.FC = () => {
   const [currentMonth, setCurrentMonth] = useState<Date>(new Date());
-  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
   const preMonth = () => {
     setCurrentMonth(subMonths(currentMonth, 1));
   };
   const nextMonth = () => {
     setCurrentMonth(addMonths(currentMonth, 1));
   };
-  const onDateClick = (day: Date) => {
+  const onDateClick = (day: Date, event: React.MouseEvent) => {
+    event.stopPropagation();
     setSelectedDate(day);
   };
+
+  const onWrapperClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setSelectedDate(null);
+  };
+
   return (
-    <ProfilCalWrapper>
+    <ProfilCalWrapper onClick={onWrapperClick}>
       <ProfileWrapper>프로필</ProfileWrapper>
       <CalendarWrapper>
         <RenderHeader
