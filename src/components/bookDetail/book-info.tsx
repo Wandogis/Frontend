78c831362975fab3861054d505362694f497aa9c
LeeHyunKyeong <115594388+LeeHyunKyeong@users.noreply.github.com>
2023-06-19 15:56:28 +0900

    [Feat] 컴포넌트 구조 변경

--- src/components/bookDetail/book-info.tsx
+++ src/components/bookDetail/book-info.tsx
@@ -84,22 +84,14 @@ const BookItem: React.FC<{ book: Book }> = ({ book }) => (
   </>
 );
 
-const BookInfo: React.FC = () => {
-  const book1: Book = {
-    title: "거인의 노트",
-    img: Book1,
-    genre: "국내도서 > 자기계발 > 성공학/경력관리",
-    author: "김익환",
-    publisher: "다산북스",
-    date: "2023년 03월 08일",
-    pages: 292,
-    summary:
-      "우리는 인생을 살면서 다양한 한계에 부딪힌다. 공부라는 외롭고 힘든 길에서 앞으로 나아가지 못하고 있다면, 아직 자신이 원하는 일을 찾지 못했다면, 10년 가까이 회사를 다녔지만 더 이상 어떻게 성과를 내야 할지 모르겠다면, 또는 아이를 키우며 반복되는 일상에 지쳐버렸거나 노년에 접어들어 은퇴를 하고 더 이상의 성장을 포기하게 되었다면, 당신은 이 한계를 넘어서길 포기한 채 그저 살던 대로 살아갈 것인가, 다시 한번 인생의 변화를 꾀할 것인가?",
-  };
+interface BookInfoProps {
+  book: Book;
+}
 
+const BookInfo: React.FC<BookInfoProps> = ({book}) => {
   return (
     <BookWrapper>
-      <BookItem book={book1} />
+      <BookItem book={book} />
     </BookWrapper>
   );
 };
